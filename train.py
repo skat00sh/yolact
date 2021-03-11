814154198b95d0bffc9149aead77fd29eefdd7df
Devendra Vyas <dvyas@ecr.ai>
2021-03-11 20:46:46 +0100

    Add pudb

--- train.py
+++ train.py
@@ -170,6 +170,8 @@ class CustomDataParallel(nn.DataParallel):
         return out
 
 def train():
+    import pudb
+    pu.db
     if not os.path.exists(args.save_folder):
         os.mkdir(args.save_folder)
 
